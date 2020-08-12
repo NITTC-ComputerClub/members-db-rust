4ac8a63ac0b94d42bdc77d149217db96fd5fa687
Naoki Ikeguchi <root@siketyan.dev>
2020-08-12 22:05:31 +0900

    Inject office365 from command line

--- src/command/add.rs
+++ src/command/add.rs
@@ -8,14 +8,19 @@ use crate::Opts;
 #[derive(Clap)]
 pub struct AddCommand {
     name: String,
+
+    #[clap(long)]
+    office365: Option<String>,
 }
 
 impl ICommand for AddCommand {
     fn execute(&self, opts: &Opts) {
         let path = String::from(&opts.database);
         let mut database = Database::new(path);
+        let member = Member::new(self.name.clone())
+            .with_office365(self.office365.clone());
 
-        database.add_member(Member::new(self.name.clone()));
+        database.add_member(member);
         database.save();
     }
 }
